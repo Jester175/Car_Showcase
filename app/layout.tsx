f8081604d739042480c2529466a5e3fd82c39feb
Nikita Kolesnew <chattrain.kol7@gmail.com>
2024-03-03 13:42:48 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="relative">{children}</body>
+    </html>
+  );
+}
